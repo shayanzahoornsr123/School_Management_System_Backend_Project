bbe094786cf9bc712d0aaef199d01f100d6e3dbd
shayanzahoornsr123 <shayanzahoornsr123@gmail.com>
2026-05-14 15:15:02 -0700

    fix firebase render deployment

--- firebase.js
+++ firebase.js
@@ -1,9 +1,11 @@
-// firebase.js
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json'); // <-- JSON file
+const admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 module.exports = admin;
